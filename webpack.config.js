98af5c051fe3cc850c146607117ad5fa24b29988
pavelusov <ipavelusov@gmail.com>
2017-09-21 23:50:39 +0500

    Added plugin: html-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,18 @@
 const path = require('path');
+const HtmlWP = require('html-webpack-plugin');
+const app = 'app';
 
 module.exports = {
-    entry: path.resolve(__dirname, 'app', 'app.js'),
+    entry: path.resolve(__dirname, app, 'app.js'),
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'bundle.js'
-    }
+    },
+    plugins: [
+        new HtmlWP({
+            title: 'Application',
+            template: path.resolve(__dirname, app, 'index.html'),
+            hash: true
+        })
+    ]
 };
